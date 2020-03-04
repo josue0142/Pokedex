0c37ce4183f9bc205e5a85547dc31b96ac33f9bb
Josue Jimenez <josuejjimenezd@gmail.com>
2020-03-04 15:58:10 -0400

    Add Database Connection, Add Context, Add Models

---
+++ Pokedex/Models/Pokemon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pokedex.Models
+{
+    public partial class Pokemon
+    {
+        public int Id { get; set; }
+        public int? TypeFk { get; set; }
+        public int? RegionFk { get; set; }
+        public string Powers { get; set; }
+
+        public virtual Region RegionFkNavigation { get; set; }
+        public virtual Type TypeFkNavigation { get; set; }
+    }
+}
---
+++ Pokedex/Models/Type.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pokedex.Models
+{
+    public partial class Type
+    {
+        public Type()
+        {
+            Pokemon = new HashSet<Pokemon>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Pokemon> Pokemon { get; set; }
+    }
+}
