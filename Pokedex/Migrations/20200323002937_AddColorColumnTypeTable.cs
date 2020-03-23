fd9ca9b163e7609500154f960c4bd471df73c472
Josue Jimenez <josuejjimenezd@gmail.com>
2020-03-23 11:47:37 -0400

    Design maintenance type complete

---
+++ Pokedex/Migrations/20200323002937_AddColorColumnTypeTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pokedex.Migrations
+{
+    public partial class AddColorColumnTypeTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "color",
+                table: "Type",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "color",
+                table: "Type");
+        }
+    }
+}
